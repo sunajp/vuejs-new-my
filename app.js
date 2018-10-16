c84d881f9f7fc645e10fd7b9213e366e93f55320
s-atsushi <suna4903@gmail.com>
2018-10-17 06:27:02 +0900

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+// ./app.js
+const vm = new Vue({
+    el: '#app',
+    data: {
+        results: [
+            {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
+            {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
+            {title: "four the last time", abstract: "lorem ipsum some test dimsum"},
+            {title: "four the last time", abstract: "lorem ipsum some test dimsum"},
+        ]
+    }
+});
---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+	KEY: "5978afce10f04c8693f642f066120970"
+}
